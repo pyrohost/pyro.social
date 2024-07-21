4bbf260a1615df3d3f315421fbf5b8dae910ea4b
fero <hi@evan.graphics>
2024-07-21 03:00:14 -0700

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,19 @@ import "./globals.css";
 const plex_mono = IBM_Plex_Mono({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://pyro.social/"),
   title: "<< pyro.social >>",
   description: "Something's cooking at Pyro.",
+  openGraph: {
+    title: "<< pyro.social >>",
+    description: "Something's cooking at Pyro.",
+    images: [
+      {
+        url: "./ogimage.png",
+      },
+    ],
+    url: "https://pyro.social",
+  },
 };
 
 export default function RootLayout({
