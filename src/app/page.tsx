03f529da718d286fc943dbd3a1c4038eddca0c25
fero <evan@pyro.host>
2024-10-18 16:48:09 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,18 +90,17 @@ export default function Home() {
           {"<<"} Pyro Engineering {">>"}
         </h2>
         <a
-          href="https://x.com/coolnickname978"
+          href="https://github.com/fetchfern"
           rel="noopener noreferrer"
           target="_blank"
           className="bg-[#373737] text-white flex items-center gap-4 w-fit px-4 py-4 min-w-[320px]"
         >
-          <svg className="w-5 h-5" viewBox="0 0 24 24" aria-hidden="true">
-            <g>
-              <path
-                d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
-                fill="currentColor"
-              ></path>
-            </g>
+          <svg className="w-5 h-5" height="16" viewBox="0 0 14 14" width="16">
+            <path
+              d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
+              fill="currentColor"
+              fillRule="nonzero"
+            ></path>
           </svg>
           <div className="flex flex-col">
             fetch
@@ -147,25 +146,24 @@ export default function Home() {
           </div>
         </a>
         <a
-          href="https://x.com/thewander02"
+          href="https://github.com/thewander02"
           rel="noopener noreferrer"
           target="_blank"
           className="bg-[#373737] text-white flex items-center gap-4 w-fit px-4 py-4 min-w-[320px]"
         >
-          <svg className="w-5 h-5" viewBox="0 0 24 24" aria-hidden="true">
-            <g>
-              <path
-                d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
-                fill="currentColor"
-              ></path>
-            </g>
+          <svg className="w-5 h-5" height="16" viewBox="0 0 14 14" width="16">
+            <path
+              d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
+              fill="currentColor"
+              fillRule="nonzero"
+            ></path>
           </svg>
           <div className="flex flex-col">
             Wander
             <span className="text-xs opacity-50">[Frontend Engineer]</span>
           </div>
         </a>
-          <a
+        <a
           href="https://x.com/notnullptr"
           rel="noopener noreferrer"
           target="_blank"
@@ -222,14 +220,24 @@ export default function Home() {
             <span className="text-xs opacity-50">[Frontend Engineer]</span>
           </div>
         </a>
-        <div
+        <a
+          href="https://github.com/he3als"
+          rel="noopener noreferrer"
+          target="_blank"
           className="bg-[#373737] text-white flex items-center gap-4 w-fit px-4 py-4 min-w-[320px]"
         >
+          <svg className="w-5 h-5" height="16" viewBox="0 0 14 14" width="16">
+            <path
+              d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
+              fill="currentColor"
+              fillRule="nonzero"
+            ></path>
+          </svg>
           <div className="flex flex-col">
             he3als
             <span className="text-xs opacity-50">[Frontend Engineer]</span>
           </div>
-        </div>
+        </a>
         <a
           href="https://x.com/YoruVR"
           rel="noopener noreferrer"
